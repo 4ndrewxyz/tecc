7d0a2365c6f515e1c329f9a0634fef97e6f3b46c
JoseeMelk <moralescastellanos1234@gmail.com>
2023-06-15 10:25:25 -0600

    cambios

--- templates/index.php
+++ templates/index.php
@@ -1,3 +1,23 @@
-<?php
-echo "safsafsa";
-?>             
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEC</title>
+</head>
+<body>
+<header class="n">
+        <nav class="navegador" id="nav-bar">
+            <h2 class="logo"><a href="/">Flor |<span>| Rama</span></a></h2>
+            <ul>
+                <li><a href="/">Inicio</a></li>
+                <li><a href="/admin">Mi cuenta</a></li>
+                <li><a href="/products">Productos</a></li>
+                <li><a href="/shoppingcart">Carrito</a></li>
+                <li><a href="#sobre-nosotros">Sobre Nosotros</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+</body>
+</html>
