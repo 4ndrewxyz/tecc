5596c56993551e0c5abac7e45069ea6ed44e25ad
Bryan Andrew Castro Valencia <123597153+4ndrewxyz@users.noreply.github.com>
2023-06-15 22:40:11 -0600

    transparent

--- templates/index.php
+++ templates/index.php
@@ -59,7 +59,7 @@
           </div>
   </div>
 </div>
-</body>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+</body>
 </html>
