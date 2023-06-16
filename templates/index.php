71bdb8c8adb948e9f9fab65be5d81752ef534ff1
Bryan Andrew Castro Valencia <123597153+4ndrewxyz@users.noreply.github.com>
2023-06-16 15:16:33 -0600

    footer

--- templates/index.php
+++ templates/index.php
@@ -25,6 +25,7 @@
 </header>
     
 <!--Seccion de carrusel-->
+<br><br><br><br><br>
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="3000">
     <div class="carousel-inner">
         <div class="carousel-item active">
