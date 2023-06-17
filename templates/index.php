94a925f24789c8306dff1bbafb7e12e9095f4022
Bryan Andrew Castro Valencia <123597153+4ndrewxyz@users.noreply.github.com>
2023-06-17 17:14:41 -0600

    test

--- templates/index.php
+++ templates/index.php
@@ -121,5 +121,6 @@
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+<script src="/public/js/mobile.js"></script>
 </body>
 </html>
