9c0b58ae594b792f33327b1f64a16cd31a5d5701
Bryan Andrew Castro Valencia <123597153+4ndrewxyz@users.noreply.github.com>
2023-06-16 23:37:57 -0600

    nuevo

--- templates/acerca-de-nosotros.php
+++ templates/acerca-de-nosotros.php
@@ -9,5 +9,20 @@
 </head>
 <body>
 <?php include 'navegacion.php'; ?>
+<br><br><br><br>
+<div class="acerca-de-nosotros">
+    <h1>Acerca de nosotros</h1>
+    <h2>¿Quiénes somos?</h2>
+    <p>Acompañarte en tus necesidades y deseos, acercándote un mundo de posibilidades para que mejores tu vida.</p>
+    <h2>Misión</h2>
+    <p>Ofrecer a nuestros clientes productos y servicios de calidad, con precios competitivos, a través de un equipo humano comprometido con la mejora continua.</p>
+    <h2>Visión</h2>
+    <p>Ser la empresa líder en el mercado de la tecnología, ofreciendo productos y servicios de calidad, con precios competitivos, a través de un equipo humano comprometido con la mejora continua.</p>
+    <h2>Valores</h2>
+    <p>Compromiso, Honestidad, Respeto, Responsabilidad, Trabajo en equipo.</p>
+</div>
+<br><br><br><br>
+
+<?php include 'footer.php'; ?>
 </body>
 </html>
